3301192bb0228d0d453760dd6e787fb485b32d4a
NoinBlake336 <josechocobar336@gmail.com>
2025-10-17 10:12:44 -0300

    17/10/2025

--- frontend/src/hooks/usePaymentsApi.js
+++ frontend/src/hooks/usePaymentsApi.js
@@ -199,7 +199,7 @@ export function useDownloadInvoiceAsPdf() {
       html2pdf()
         .set({
           margin: 0.5,
-          filename: `comprobante-${factura.numero}.pdf`,
+          filename: `${factura.cae}-${factura.numero}.pdf`,
           image: { type: "jpeg", quality: 0.98 },
           html2canvas: { scale: 2 },
           jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
